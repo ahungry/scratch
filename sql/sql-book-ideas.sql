607259a78dca986d43774ff0df8f14b52f849845
Matthew Carter <m@ahungry.com>
2019-04-04 01:34:05 -0400

    Add some ideas for how we could wrap QueryFile things..

---
+++ sql/sql-book-ideas.sql
@@ -0,0 +1,9 @@
+-- See if we can make a polyglot SQL
+PREPARE fooplan (int, text, bool, numeric, text) AS
+  SELECT $1 as my_int
+  , $2 as my_text
+  , $3 as my_bool
+  , $4 as my_numeric
+  , $5::int as cast_int
+  ;
+EXECUTE fooplan(1, 'Hunter Valley', 't', 200.00, '42');
