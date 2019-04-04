a54771a74c90cb6a93640cd8a2c42b282cd199e0
Matthew Carter <m@ahungry.com>
2019-04-04 01:38:47 -0400

    Add some ideas

--- sql/sql-book-ideas.sql
+++ sql/sql-book-ideas.sql
@@ -1,4 +1,4 @@
--- See if we can make a polyglot SQL
+-- We could also ensure we never run line 1 by the tool
 PREPARE fooplan (int, text, bool, numeric, text) AS
   SELECT $1 as my_int
   , $2 as my_text
@@ -6,5 +6,9 @@ PREPARE fooplan (int, text, bool, numeric, text) AS
   , $4 as my_numeric
   , $5::int as cast_int
   ;
+
+-- We could easily have everything after some token in the file ignored by a QueryLoader
+-- Essentially using it as some type of spec.
 --explain analyze
 EXECUTE fooplan(1, 'Hunter Valley', 't', 200.00, '42');
+EXECUTE fooplan(5, 'Fake Valley', 'f', 400.04, '82');
