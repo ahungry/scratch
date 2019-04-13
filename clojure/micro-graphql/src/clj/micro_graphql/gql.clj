99b2214f3865c02492277792615d4ca76aa7d94e
Matthew Carter <m@ahungry.com>
2019-04-12 21:53:05 -0400

    Tinker with gql server - not bad

--- clojure/micro-graphql/src/clj/micro_graphql/gql.clj
+++ clojure/micro-graphql/src/clj/micro_graphql/gql.clj
@@ -3,6 +3,7 @@
    [graphql-clj.executor :as executor]))
 
 (def schema-str "type User {
+    byName(s: String): String
     name: String
     age: Int
   }
@@ -22,6 +23,40 @@
                                   :age 30})}}
           [type-name field-name]))
 
-(def query-str "query {user {name age}}")
+(defn roll-n-dice-n-times [sides times]
+  (map (fn [_] (rand-int sides)) (range times)))
+
+(defn handler-user [field-name]
+  (fn [ctx p args]
+    (prn "User contexts: " ctx p args)
+    (get p (keyword field-name))))
+
+(defn handler-query-dice [field-name]
+  (case field-name
+    "rollDice" (fn [c p a] (roll-n-dice-n-times (get a "numSides") (get a "numDice")))))
+
+(defn handler-root [field-name]
+  (case field-name
+    ;; Basically chains the data downwards.
+    "user" (fn [c p a] {:name "Matt" :age 36})
+    (fn [c p a] p)))
+
+;; It basically works down the query list, like a reduce call.
+(defn resolver-fn-dispatcher [type-name field-name]
+  (prn type-name field-name)
+  (case type-name
+    "QueryRoot" (handler-root field-name)
+    "QueryDice" (handler-query-dice field-name)
+    "User" (handler-user field-name)
+    (fn [ctx p args] p)
+    ))
+
+;; https://graphql.org/graphql-js/passing-arguments/
+(def query-str (slurp "query.gql"))
 
 (executor/execute nil schema-str resolver-fn query-str)
+
+(defn test-dice []
+  (let [schema (slurp "gql/schema/dice.gql")
+        query (slurp "gql/query/dice.gql")]
+    (executor/execute nil schema resolver-fn-dispatcher query)))
