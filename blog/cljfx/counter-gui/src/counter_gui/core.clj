eeba7e4908e21e2d362f56713a71c940ecb47921
Matthew Carter <m@ahungry.com>
2019-07-14 00:57:53 -0400

    Different handlers working with state

--- blog/cljfx/counter-gui/src/counter_gui/core.clj
+++ blog/cljfx/counter-gui/src/counter_gui/core.clj
@@ -16,20 +16,33 @@
     ::stub (update-in state [:clicked] inc-or-make)
     state))
 
-(def bws-prefix ::make-me-random)
+(defn make-button-with-state
+  "Wrapper to generate a stateful widget."
+  [prefix]
+  (let [handler (fn [event state]
+                  "The event dispatching for received events."
+                  [event state]
+                  (if (= prefix (:prefix event))
+                    (case (:event/type event)
+                      ::clicked (update-in state [prefix :clicked] inc-or-make)
+                      state)
+                    state))
+        view (fn [state]
+               (let [{:keys [clicked]} (prefix state)]
+                 {:fx/type :button
+                  :on-action {:event/type ::clicked
+                              :prefix prefix}
+                  :text (str "Click me more! x " clicked prefix)}))]
+    ;; Send the handler and view back up to the caller.
+    {:handler handler
+     :view view}))
 
-(defn eh-bws-1 [event state]
-  (case (:event/type event)
-    ::clicked (update-in state [bws-prefix :clicked] inc-or-make)
-    state))
-
-(defn button-with-state [state]
-  (let [{:keys [clicked]} (bws-prefix state)]
-    {:fx/type :button
-     :on-action {:event/type ::clicked}
-     :text (str "Click me more! x " clicked)}))
-
-(def event-handlers [event-handler eh-bws-1])
+(def bws-1 (make-button-with-state ::bws-1))
+(def bws-2 (make-button-with-state ::bws-2))
+(def event-handlers
+  [event-handler
+   (:handler bws-1)
+   (:handler bws-2)])
 
 (defn run-event-handlers
   "If we have many event handler layers, we want to run each one in sequence.
@@ -52,8 +65,8 @@
                   :children
                   [
                    {:fx/type :label :text (str "Root state is: " clicked)}
-                   (button-with-state state)
-                   ;; {:fx/type button-with-state}
+                   ((:view bws-1) state)
+                   ((:view bws-2) state)
                    ]}
            }
 
