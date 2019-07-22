3458fd0957e62e197fac9f3643658c168bfeea70
Matthew Carter <m@ahungry.com>
2019-07-21 23:26:06 -0400

    Set up dockerfile/makefile samples

---
+++ blog/cljfx/counter-gui/run-docker.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+[[ -S /tmp/pulseaudio.xsocket ]] || pactl load-module module-native-protocol-unix socket=/tmp/pulseaudio.socket
+
+xhost +"local:docker@"
+xhost +local:root
+
+docker run -it --rm \
+       -u 1000 \
+       -e DISPLAY=$DISPLAY \
+       -e NVIDIA_DRIVER_CAPABILITIES=compute,utility,graphics \
+       -v /tmp/.X11-unix:/tmp/.X11-unix \
+       -v /dev/snd:/dev/snd \
+       -v /tmp/pulseaudio.socket:/tmp/pulseaudio.socket \
+       -v /home/mcarter/src/eq-in-a-docker/pulseaudio.client.conf:/etc/pulse/client.conf \
+       --device=/dev/snd:/dev/snd \
+       --cpus=2 \
+       --privileged \
+       --name counter-gui_running \
+       counter-gui:latest
