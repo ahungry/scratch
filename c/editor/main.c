2186d1fb522567c408c5d6836bd60978994ad51e
Matthew Carter <m@ahungry.com>
2019-08-01 23:05:09 -0400

    Try out my own goto code line

--- c/editor/main.c
+++ c/editor/main.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+#include <math.h>
 
 /** defines **/
 
@@ -17,10 +18,40 @@
 
 struct termios orig_termios;
 
+/** util **/
+
+// COL / ROW
+void die (const char *s);
+
+// https://stackoverflow.com/questions/8257714/how-to-convert-an-int-to-string-in-c#8257728
+int get_byte_size_of_int_as_char (int n)
+{
+  return (int) ((ceil (log10 (n)) + 1) * sizeof (char));
+}
+
+void cursor_goto (int x, int y)
+{
+  int xlen = get_byte_size_of_int_as_char (x);
+  int ylen = get_byte_size_of_int_as_char (y);
+  int olen = 4; // \x1b [ H, and the ;
+  char str[xlen + ylen + olen]; // Slot for \x1b[_;_H
+
+  sprintf (str, "\x1b[%d;%dH", x, y);
+  write (STDOUT_FILENO, "\x1b[H", 3); // Position cursor 12;40H would center on an 80x24 size
+}
+
+void clear_and_reposition ()
+{
+  write (STDOUT_FILENO, "\x1b[2J", 4); // Clear screen
+  cursor_goto (1, 1);
+  write (STDOUT_FILENO, "\x1b[H", 3); // Position cursor 12;40H would center on an 80x24 size
+}
+
 /** terminal **/
 
 void die (const char *s)
 {
+  clear_and_reposition ();
   perror (s);
   exit (1);
 }
@@ -60,6 +91,24 @@ char editor_read_key ()
   return c;
 }
 
+/** output **/
+void editor_draw_rows ()
+{
+  int y;
+
+  for (y = 0; y < 24; y++)
+    {
+      write (STDOUT_FILENO, "~\r\n", 3);
+    }
+}
+
+void editor_refresh_screen ()
+{
+  clear_and_reposition ();
+  editor_draw_rows ();
+  cursor_goto (1, 1);
+}
+
 /** input **/
 void editor_process_keypress ()
 {
@@ -69,6 +118,7 @@ void editor_process_keypress ()
   switch (c)
     {
     case CTRL_KEY('q'):
+      clear_and_reposition ();
       exit (0);
       break;
     }
@@ -76,13 +126,13 @@ void editor_process_keypress ()
 
 
 /** init **/
-
 int main ()
 {
   enable_raw_mode ();
 
   while (1)
     {
+      editor_refresh_screen ();
       editor_process_keypress ();
     }
 
