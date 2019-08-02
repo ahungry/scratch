bcdb35094431e85b83faab60f3b278b2adcc72ed
Matthew Carter <m@ahungry.com>
2019-08-02 15:58:13 -0400

    And we can send a udp and get back a reply before leaving

--- c/udp_client.c
+++ c/udp_client.c
@@ -12,11 +12,11 @@
 #include <netinet/in.h>
 #include <sys/uio.h>
 
+void send_udp (int fd, struct addrinfo* res);
+
 void die (const char *s) { perror (s); exit (1); }
 
-// Fire and forget some udp (we don't get anything back)
-// I guess we would have to connect() or wait to read a response from the remote?
-void send_udp ()
+int get_socket_fd (struct addrinfo** return_res)
 {
   const char* hostname = "127.0.0.1"; /* localhost */
   const char* portname = "12345";
@@ -31,6 +31,25 @@ void send_udp ()
   if (err != 0) die("getaddrinfo");
   int fd = socket (res->ai_family, res->ai_socktype, res->ai_protocol);
   if (fd == -1) die("socket");
+
+  // Return the res by setting in place
+  *return_res = res;
+
+  /* const char content[30] = "Hello world"; */
+  /* if (sendto (fd, content, sizeof (content), 0, */
+  /*             res->ai_addr, res->ai_addrlen) == -1) */
+  /*   { */
+  /*     die("sendto"); */
+  /*   } */
+
+  // send_udp (fd, res);
+
+  return fd;
+}
+
+// Fire and forget some udp (we don't get anything back)
+void send_udp (int fd, struct addrinfo* res)
+{
   const char content[30] = "Hello world";
   if (sendto (fd, content, sizeof (content), 0,
               res->ai_addr, res->ai_addrlen) == -1)
@@ -39,8 +58,25 @@ void send_udp ()
     }
 }
 
+// Receive a udp we previously sent out
+void receive_udp (int fd, struct addrinfo* res)
+{
+  int n;
+  char buf[100];
+  uint len;
+  len = sizeof (res);
+  n = recvfrom (fd, buf, 100, 0, (struct sockaddr *) &res, &len);
+
+  if (n < 0) die("recvfrom");
+  printf ("Got %d bytes\n", n);
+}
+
 int main ()
 {
-  send_udp ();
-  exit (0);
+  struct addrinfo* res = 0;
+  int fd = get_socket_fd (&res);
+  send_udp (fd, res);
+  receive_udp (fd, res);
+
+  return 0;
 }
