c02cbb455d24b31cbd3544cb86592a2fd0dd3e80
Matthew Carter <m@ahungry.com>
2019-08-02 14:17:27 -0400

    Workign client

--- c/udp_client.c
+++ c/udp_client.c
@@ -1,119 +1,46 @@
 // Start a process, send a single UDP to somewhere, and end.
 // http://www.microhowto.info/howto/send_a_udp_datagram_in_c.html
 
-/* UDP echo server program -- udp_client.c */
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/uio.h>
 
-#include <stdio.h>      /* standard C i/o facilities */
-#include <stdlib.h>     /* needed for atoi() */
-#include <unistd.h>     /* defines STDIN_FILENO, system calls,etc */
-#include <sys/types.h>  /* system data type definitions */
-#include <sys/socket.h> /* socket specific definitions */
-#include <netinet/in.h> /* INET constants and stuff */
-#include <arpa/inet.h>  /* IP address conversion stuff */
-#include <netdb.h>      /* gethostbyname */
+void die (const char *s) { perror (s); exit (1); }
 
-
-
-/* this routine echos any messages (UDP datagrams) received */
-
-#define MAXBUF 1024*1024
-
-void send_packet ()
+// Fire and forget some udp (we don't get anything back)
+// I guess we would have to connect() or wait to read a response from the remote?
+void send_udp ()
 {
-  int ld;
-  struct sockaddr_in skaddr;
-  uint length;
-
-  /* create a socket
-     IP protocol family (PF_INET)
-     UDP protocol (SOCK_DGRAM)
-  */
-
-  if ((ld = socket( PF_INET, SOCK_DGRAM, 0 )) < 0) {
-    printf("Problem creating socket\n");
-    exit(1);
-  }
-
-  skaddr.sin_family = AF_INET;
-  skaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  skaddr.sin_port = htons(12345); // 0 for the kernel to choose random
-}
-
-void echo( int sd ) {
-    uint len;
-    int n;
-    char bufin[MAXBUF];
-    struct sockaddr_in remote;
-
-    /* need to know how big address struct is, len must be set before the
-       call to recvfrom!!! */
-
-    len = sizeof(remote);
-
-    while (1) {
-      /* read a datagram from the socket (put result in bufin) */
-      n=recvfrom(sd,bufin,MAXBUF,0,(struct sockaddr *)&remote,&len);
-
-      /* print out the address of the sender */
-      printf("Got a datagram from %s port %d\n",
-             inet_ntoa(remote.sin_addr), ntohs(remote.sin_port));
-
-      if (n<0) {
-        perror("Error receiving data");
-      } else {
-        printf("GOT %d BYTES\n",n);
-        /* Got something, just send it back */
-        sendto(sd,bufin,n,0,(struct sockaddr *)&remote,len);
-      }
+  const char* hostname = "127.0.0.1"; /* localhost */
+  const char* portname = "12345";
+  struct addrinfo hints;
+  memset (&hints, 0, sizeof (hints));
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_DGRAM;
+  hints.ai_protocol = 0;
+  hints.ai_flags = AI_ADDRCONFIG;
+  struct addrinfo* res = 0;
+  int err = getaddrinfo (hostname, portname, &hints, &res);
+  if (err != 0) die("getaddrinfo");
+  int fd = socket (res->ai_family, res->ai_socktype, res->ai_protocol);
+  if (fd == -1) die("socket");
+  const char content[30] = "Hello world";
+  if (sendto (fd, content, sizeof (content), 0,
+              res->ai_addr, res->ai_addrlen) == -1)
+    {
+      die("sendto");
     }
 }
 
-/* server main routine */
-
-int main() {
-  int ld;
-  struct sockaddr_in skaddr;
-  uint length;
-
-  /* create a socket
-     IP protocol family (PF_INET)
-     UDP protocol (SOCK_DGRAM)
-  */
-
-  if ((ld = socket( PF_INET, SOCK_DGRAM, 0 )) < 0) {
-    printf("Problem creating socket\n");
-    exit(1);
-  }
-
-  /* establish our address
-     address family is AF_INET
-     our IP address is INADDR_ANY (any of our IP addresses)
-     the port number is assigned by the kernel
-  */
-
-  skaddr.sin_family = AF_INET;
-  skaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  skaddr.sin_port = htons(12345); // 0 for the kernel to choose random
-
-  if (bind(ld, (struct sockaddr *) &skaddr, sizeof(skaddr))<0) {
-    printf("Problem binding\n");
-    exit(0);
-  }
-
-  /* find out what port we were assigned and print it out */
-
-  length = sizeof( skaddr );
-  if (getsockname(ld, (struct sockaddr *) &skaddr, &length)<0) {
-    printf("Error getsockname\n");
-    exit(1);
-  }
-
-  /* port number's are network byte order, we have to convert to
-     host byte order before printing !
-  */
-  printf("The server UDP port number is %d\n",ntohs(skaddr.sin_port));
-
-  /* Go echo every datagram we get */
-  echo(ld);
-  return(0);
+int main ()
+{
+  send_udp ();
+  exit (0);
 }
