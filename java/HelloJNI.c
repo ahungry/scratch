9c5d7e361c5566bb33926c9f7a6ccaccddaa914b
Matthew Carter <m@ahungry.com>
2019-09-07 00:13:51 -0400

    Working construction

--- java/HelloJNI.c
+++ java/HelloJNI.c
@@ -1,10 +1,30 @@
 // Save as "HelloJNI.c"
 #include <jni.h>        // JNI header provided by JDK
+#include <string.h>
+#include <stdlib.h>
 #include <stdio.h>      // C Standard IO Header
 #include "HelloJNI.h"   // Generated
 
 // Implementation of the native method sayHello()
-JNIEXPORT void JNICALL Java_HelloJNI_sayHello(JNIEnv *env, jobject thisObj) {
-   printf("Hello World!\n");
-   return;
+JNIEXPORT void
+
+JNICALL
+Java_HelloJNI_sayHello (JNIEnv *env, jobject thisObj)
+{
+  printf ("Hello World!\n");
+  return;
+}
+
+JNIEXPORT jstring
+
+JNICALL
+Java_HelloJNI_addOne (JNIEnv *env, jobject thisObj, jint y)
+{
+  int a = 1;
+  char *msg = NULL;
+  msg = (char *) malloc (sizeof (char) * 40);
+  sprintf (msg, "%d", a + y);
+
+  // return env->NewStringUTF (msg);
+  return (*env)->NewStringUTF (env, msg);
 }
--- java/HelloJNI.java
+++ java/HelloJNI.java
@@ -7,9 +7,14 @@ public class HelloJNI {
 
   // Declare an instance native method sayHello() which receives no parameter and returns void
   private native void sayHello();
+  private native String addOne(int y);
 
   // Test Driver
   public static void main(String[] args) {
     new HelloJNI().sayHello();  // Create an instance and invoke the native method
+
+    String answer = new HelloJNI().addOne(2);
+    System.out.println(answer);
+    System.out.println("All done?");
   }
 }
