eb9a29dddff96325e140b7a7ab5ae0c38388bc71
Matthew Carter <m@ahungry.com>
2019-09-12 01:37:20 -0400

    Some interesting tinkering....

---
+++ composed-types/ct.js
@@ -0,0 +1,42 @@
+// This would be hidden implementation code.
+// Define a couple types
+const Num = Symbol('n')
+const Str = Symbol('s')
+
+// Holds a function with typing info included
+const fn = (i, o, f) => ({ i, o, f })
+
+// Call the function (unary arity atm, although this could be dynamic)
+const invoke = ({ f }) => (x) => f(x)
+const in_type = ({ i }) => i
+const out_type = ({ o }) => o
+
+const err = (_i, _o) => { throw new Error('Incompatible types!') }
+const assert_types = (f, g) => in_type(f) === out_type(g) ? true : err(in_type(f), out_type(g))
+const comp = (f, g) => assert_types(f, g) ?
+  fn(out_type(g), in_type(f), (x) => invoke(f)(invoke(g)(x))) :
+  false
+
+const compmap = (f, g) => assert_types(f, g) ?
+  fn(out_type(g), in_type(g), x => invoke(g)(x).map(y => invoke(f)(y))) :
+  false
+
+// This would be the user space code.
+
+// Make an instance of a 'typed' function
+const add_one = fn(Num, Num, n => n + 1)
+const stringer = fn(Num, Str, n => "Your number is: " + String(n))
+
+console.log(invoke(add_one)(3)) // 4
+console.log(invoke(stringer)(3)) // "Your number is: 3"
+
+const add_two = comp(add_one, add_one)
+console.log(invoke(add_two)(10)) // evals to 12
+
+// This will throw an error about invalid composition, as we can't add one to a string
+// This could be more lenient and just print a warning etc.
+// const add_bad = comp(add_one, stringer)
+
+const range = fn(Num, Num, (n) => [...Array(n).keys()])
+const add_two_to_list = compmap(add_two, range)
+console.log(invoke(add_two_to_list)(3)) // produces [2, 3, 4]
---
+++ composed-types/ct.scm
@@ -0,0 +1,81 @@
+;; Load at REPL via ,use (oop goops
+(use-modules (oop goops))
+
+;; Main conversion types...
+;; string->number
+;; number->string
+
+(define (foo n) n)
+
+(define (fn i o f)
+  `((i . ,i)
+    (o . ,o)
+    (f . ,f)))
+
+(define add-one (fn 'number 'number (lambda (n) (+ 1 n))))
+(define stringer (fn 'number 'string (lambda (n) (number->string n))))
+
+(define mode 'type-check)
+(define (type-check) (set! mode 'type-check))
+(define (runtime) (set! mode 'runtime))
+(define (invoke f x) ((cdr (assoc 'f f)) x))
+(define (in-type f) (cdr (assoc 'i f)))
+(define (out-type f) (cdr (assoc 'o f)))
+
+;; This equal? comparison could be vastly improved to allow hierarchal types
+;; as well as checking multiple types vs singular types.
+(define (assert-types f g)
+  (if (equal? (in-type f) (out-type g))
+      #t
+      (error (format #nil "Incompatible types in comp: expected ~a got ~a~%" (in-type f) (out-type g)))))
+
+(define (_comp f g)
+  (fn (out-type f) (in-type g)
+      (lambda (x) (invoke f (invoke g x)))))
+
+(define (comp f g)
+  (if (equal? 'type-check mode)
+      (begin (assert-types f g) (_comp f g))
+      (_comp f g)))
+
+(define (_range count iter ns)
+  (if (>= (length ns) count)
+      ns
+      (_range count (+ 1 iter) (cons iter ns))))
+
+;; Should we define a collection type? OR always treat a collection element
+;; as if it evalled to a single element and flag a col type differently...
+(define range (fn 'number 'number (lambda (n) (reverse (_range n 0 #nil)))))
+
+;; Will compose just fine.
+(define add-one->string (comp stringer add-one))
+(define add-2 (comp add-one add-one))
+(invoke add-2 3)                        ; produces 5
+
+;; Even the comp of a comped fn will ensure type safety.
+(define add-4 (comp add-2 add-2))
+(invoke add-4 10)                       ; produces 14
+
+;;  Will trigger an error.
+;; (define add-one-to-string (comp add-one stringer))
+
+;; How about a type safe map facility? Comp a fn f to a data provider g
+;; To ultimately generate a 0 arity fn to invoke later.
+(define (_compmap f g)
+  (fn (out-type g) (in-type g)
+      (lambda (x) (map (lambda (n) (invoke f n)) (invoke g x)))))
+(define (compmap f g)
+  (if (equal? 'type-check mode)
+      (begin (assert-types f g) (_compmap f g))
+      (_compmap f g)))
+
+(define add-4-to-range (compmap add-4 range))
+
+;; evals to 4 5 6
+(invoke add-4-to-range 3)
+
+;; Now, a potential fail case...
+(define get-names (fn 'nil 'string (lambda (_) (list "Jon" "Bob"))))
+
+;; Incompatible types in comp: expected number got string
+;; (define add-4-to-names (compmap add-4 get-names))
