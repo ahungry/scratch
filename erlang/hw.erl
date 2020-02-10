13177df3ef4bc81c834bcdbb0874cb21d15044ac
Matthew Carter <m@ahungry.com>
2020-02-09 23:45:45 -0500

    add some sample recursive and TCO ones

--- erlang/hw.erl
+++ erlang/hw.erl
@@ -100,7 +100,21 @@ insert(X,Set) ->
     false -> [X|Set]
   end.
 
-fac(N) when N == 0 -> 1;
+fac(0) -> 1;
 fac(N) when N > 0 -> N * fac(N-1).
 
 %% https://learnyousomeerlang.com/recursion
+
+%% writing a length function
+len([]) -> 0;
+len([_|T]) -> 1 + len(T).
+
+tail_fac(N) -> tail_fac(N, 1).
+
+tail_fac(0, Acc) -> Acc;
+tail_fac(N, Acc) when N > 0 -> tail_fac(N-1, N*Acc).
+
+tail_len(L) -> tail_len(L, 0).
+
+tail_len([], Acc) -> Acc;
+tail_len([_|T], Acc) -> tail_len(T, 1 + Acc).
