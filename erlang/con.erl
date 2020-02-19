c3ab3846760ff7043a4383c27d69e2afabb0bdd7
Matthew Carter <m@ahungry.com>
2020-02-19 10:38:43 -0500

    A way to have two processes talk to each other

--- erlang/con.erl
+++ erlang/con.erl
@@ -3,17 +3,34 @@
 -module(con).
 -compile(export_all).
 
-dolphin3() ->
+dolphin3(RecvPid) ->
     receive
         {From, do_a_flip} ->
             From ! "How about no?",
-            dolphin3();
+            RecvPid ! "How about no? (recv)",
+            dolphin3(RecvPid);
+
         {From, fish} ->
-            From ! "So long and thanks for all the fish!";
+            From ! "So long and thanks for all the fish!",
+            RecvPid ! "So long and thanks for all the fish! (recv)";
+
         _ -> io:format("Heh, we're smarter than you humans.~n"),
-             dolphin3()
+             dolphin3(RecvPid)
     end.
 
 %% See whats pending with flush()
 %% Make a new pid with Pid=spawn(module_name, function_name, [init_args])
 %% Rest vars with f()
+
+receiver() ->
+    receive
+        Anything -> io:format("We got something from dolphin pid! ~p~n", [Anything]),
+                    receiver()
+    end.
+
+test_d3() ->
+    RecvPid = spawn(con, receiver, []),
+    DolphinPid = spawn(con, dolphin3, [RecvPid]),
+    DolphinPid ! {self(), do_a_flip},
+    DolphinPid ! {self(), fish},
+    DolphinPid ! {self(), terminate}.
