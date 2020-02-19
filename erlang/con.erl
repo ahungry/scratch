47cf16e3442432652b43208cb8f74ab6277a4f39
Matthew Carter <m@ahungry.com>
2020-02-19 10:18:18 -0500

    add some notes

--- erlang/con.erl
+++ erlang/con.erl
@@ -1,3 +1,5 @@
+%% http://erlang.ahungry.com/the-hitchhikers-guide-to-concurrency.html#dont-panic
+
 -module(con).
 -compile(export_all).
 
@@ -11,3 +13,7 @@ dolphin3() ->
         _ -> io:format("Heh, we're smarter than you humans.~n"),
              dolphin3()
     end.
+
+%% See whats pending with flush()
+%% Make a new pid with Pid=spawn(module_name, function_name, [init_args])
+%% Rest vars with f()
