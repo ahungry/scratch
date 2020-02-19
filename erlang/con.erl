33cc8d765d7e7d524bdfdb5e69a3ff95047a3533
Matthew Carter <m@ahungry.com>
2020-02-19 11:06:39 -0500

    Sample to accumulate parallel responses

--- erlang/con.erl
+++ erlang/con.erl
@@ -22,15 +22,24 @@ dolphin3(RecvPid) ->
 %% Make a new pid with Pid=spawn(module_name, function_name, [init_args])
 %% Rest vars with f()
 
-receiver() ->
+receiver(Acc, ExpectedCount, FinalPid) when length(Acc) < ExpectedCount ->
     receive
         Anything -> io:format("We got something from dolphin pid! ~p~n", [Anything]),
-                    receiver()
-    end.
+                    receiver([Anything|Acc], ExpectedCount, FinalPid)
+    end;
+receiver(Acc, _, FinalPid) -> FinalPid ! {final, Acc}.
 
 test_d3() ->
-    RecvPid = spawn(con, receiver, []),
+    FinalPid = self(),
+    RecvPid = spawn(con, receiver, [[], 2, FinalPid]),
     DolphinPid = spawn(con, dolphin3, [RecvPid]),
     DolphinPid ! {self(), do_a_flip},
     DolphinPid ! {self(), fish},
-    DolphinPid ! {self(), terminate}.
+    DolphinPid ! {self(), terminate},
+    %% This will stall out and wait for the match - I guess it could fill up
+    %% with message passes we don't actually care about since we aren't handling
+    %% then in our receive block - I guess we would want to put this in its own spawn
+    %% later.
+    receive
+        {final, Anything} -> io:format("At the end, FinalPid called! ~p~n", [Anything])
+    end.
