6bdc00f2704bd5ffd76433587747e9ded5e50e65
Matthew Carter <m@ahungry.com>
2020-03-21 00:32:32 -0400

    Nice and simple (for any language that has JSON encode/decode at least)

--- langless-web/app/cgi-bin/index.sh
+++ langless-web/app/cgi-bin/index.sh
@@ -4,20 +4,20 @@ echo -ne "Content-Type: application/json\n\n"
 
 # env
 if [ "$REQUEST_METHOD" != 'POST' ]; then
-    echo "select * from users" | sqlite3 /tmp/users.db
+    echo "select * from users" | sqlite3 -csv /tmp/users.db
     echo fin
     exit 0
 fi
 
 json=$(cat <&0)
 
-user=$(echo $json | jq .user)
-pass=$(echo $json | jq .pass)
+user=$(echo $json | jq .user | sed -e 's/"//g')
+pass=$(echo $json | jq .pass | sed -e 's/"//g')
 
 result='false'
 
 # Validation
-if [ -z "$user" -o -z "$pass" -o "null" = "$user" -o "null" = "$pass" ]; then
+if [ -z "$user" -o -z "$pass" -o '"null"' = "$user" -o '"null"' = "$pass" ]; then
     result='false'
 else
     result='true'
@@ -26,14 +26,8 @@ fi
 # Persistence
 sqlite3 /tmp/users.db "INSERT INTO users (user, pass) VALUES ('$user', '$pass')" 2>&1
 
-sqlite3 /tmp/users.db "INSERT INTO users (user, pass) VALUES ('user', 'pass')" 2>&1
-#echo DO INSERT
-#sqlite3 /tmp/users.db "INSERT INTO users (user, pass) VALUES ('user', 'pass')"
-#sqlite3 /tmp/users.db INSERT INTO users (user, pass) VALUES ('user', 'pass')
-#echo DONE
-
 cat <<EOF
-{"success": $result, "user": ${user}, "pass": ${pass}}
+{"success": $result, "user": "${user}", "pass": "${pass}"}
 EOF
 
 exit 0
