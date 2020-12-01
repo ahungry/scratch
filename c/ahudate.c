70b1b259208c8118283309ffd9caf40e7d382426
Matthew Carter <m@ahungry.com>
2020-11-30 23:00:26 -0500

    clean up some comments

--- c/ahudate.c
+++ c/ahudate.c
@@ -92,35 +92,13 @@ make_ahudate_mask (char *s)
 }
 
 int
-ahudate_is_yyyy_mm_dd (char *s, char sep)
+ahudate_is_yyyy_mm_dd (char *s)
 {
-  /* int mask[] = { is_numeric, is_numeric, is_numeric, is_numeric, is_separator }; */
-
-  /* return xahudate_is_yyyy_mm_dd ("2020/10/10", mask, 2); */
   ahudate_mask_t * m = make_ahudate_mask ("dddd/dd/dd");
+  int n = ahudate_is_mask_valid (s, m->mask, m->size);
+  free (m);
 
-  return ahudate_is_mask_valid ("2020/10/10", m->mask, m->size);
-  // return xahudate_is_yyyy_mm_dd ("2020/1/10", m->mask, m->size);
-
-  /* for (int i = 0; i < (int) strlen (s); i++) */
-  /*   { */
-  /*     if ((i == 4 || i == 7) && s[i] == sep) */
-  /*       { */
-  /*         continue; */
-  /*       } */
-
-  /*     if (! ahudate_is_numeric (s[i])) */
-  /*       { */
-  /*         return 0; */
-  /*       } */
-
-  /*     if (i >= 9) */
-  /*       { */
-  /*         return 1; */
-  /*       } */
-  /*   } */
-
-  /* return 0; */
+  return n;
 }
 
 int
@@ -147,43 +125,23 @@ ahudate_get_year (char *s, char *year)
 int
 main (int argc, char *argv[])
 {
-  char year[4];
-
-  if (strstr (argv[1], "/"))
+  for (int i = 1; i < argc; i++)
     {
-      fprintf (stderr, "Slash formatted");
-      ahudate_get_year (argv[1], year);
+      char year[4];
+
+      if (! ahudate_is_yyyy_mm_dd (argv[i]))
+        {
+          fprintf (stderr, "\nInvalid input: %s\n", argv[i]);
+          continue;
+        }
+      fprintf (stderr, "\nValid input: %s\n", argv[i]);
+
+      ahudate_get_year (argv[i], year);
+
+      fprintf (stderr, "Year was:%s\n", year);
+      fprintf (stderr, "Was it valid?: %d\n", ahudate_is_yyyy_mm_dd (argv[i]));
+      fprintf (stderr, "To epoch: %ld\n", ahudate_year_to_epoch (year));
     }
 
-  fprintf (stderr, "Hello World\n");
-  fprintf (stderr, "Year was:%s\n", year);
-  fprintf (stderr, "Was it valid?: %d\n", ahudate_is_yyyy_mm_dd (argv[1], '/'));
-  fprintf (stderr, "To epoch: %ld\n", ahudate_year_to_epoch (year));
-
   exit (EXIT_SUCCESS);
-
-  /* struct tm *tmp; */
-
-  /* for (int j = 1; j < argc; j++) { */
-  /*   tmp = getdate(argv[j]); */
-
-  /*   if (tmp == NULL) { */
-  /*     printf("Call %d failed; getdate_err = %d\n", */
-  /*            j, getdate_err); */
-  /*     continue; */
-  /*   } */
-
-  /*   printf("Call %d (\"%s\") succeeded:\n", j, argv[j]); */
-  /*   printf("    tm_sec   = %d\n", tmp->tm_sec); */
-  /*   printf("    tm_min   = %d\n", tmp->tm_min); */
-  /*   printf("    tm_hour  = %d\n", tmp->tm_hour); */
-  /*   printf("    tm_mday  = %d\n", tmp->tm_mday); */
-  /*   printf("    tm_mon   = %d\n", tmp->tm_mon); */
-  /*   printf("    tm_year  = %d\n", tmp->tm_year); */
-  /*   printf("    tm_wday  = %d\n", tmp->tm_wday); */
-  /*   printf("    tm_yday  = %d\n", tmp->tm_yday); */
-  /*   printf("    tm_isdst = %d\n", tmp->tm_isdst); */
-  /* } */
-
-  /* exit(EXIT_SUCCESS); */
 }
