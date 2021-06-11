6492f952a22aa654d1712cfafbb295aaed3128a6
Matthew Carter <m@ahungry.com>
2021-06-10 22:48:43 -0400

    Working - interesting

--- js/syncHttp.js
+++ js/syncHttp.js
@@ -1,15 +1,38 @@
 // Just a test to do a synchronous http call
 const http = require('http')
 
-void async function main () {
-  const data = await new Promise((resolve, reject) => {
+// see: https://www.npmjs.com/package/sleep
+async function doAsyncRequest (url) {
+  return new Promise((resolve, reject) => {
     http
-      .request('http://example.com', res => {
+      .request(url, res => {
         res.on('data', resolve)
         res.on('error', reject)
       })
       .on('error', reject)
       .end('')
   })
-  console.log(data.toString())
+}
+
+function doSyncRequest (url, f) {
+  let data = null
+
+  doAsyncRequest(url, f).then(x => { data = x })
+
+  while (!data) {
+    require('deasync').sleep(100)
+  }
+
+  return data.toString()
+}
+
+// void async function main () {
+void function main () {
+  console.log('Begin')
+  // await doAsyncRequest('http://httpbin.org/ip')
+  const x = doSyncRequest('http://httpbin.org/ip', x => x)
+  console.log('data was: ', { x })
+  console.log('after req')
+  console.log('after sleep')
+  console.log('End')
 }()
