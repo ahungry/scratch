acae4ba78ef2bfd8453889869c0936d94fc934b8
Matthew Carter <m@ahungry.com>
2022-02-24 23:20:31 -0500

    More tweaks

--- k8s/server.js
+++ k8s/server.js
@@ -1,9 +1,15 @@
-var http = require('http');
+var http = require('http')
+
+// Intentionally running a stateful thing (no-no) to illustrate
+// the load balancing feature of this.
+var counter = 0
 
 var handleRequest = function(request, response) {
-	  console.log('Received request for URL: ' + request.url);
-	  response.writeHead(200);
-	  response.end('Hello World from localhost!');
-};
-var www = http.createServer(handleRequest);
-www.listen(8080);
+  counter++
+	console.log('Received request for URL: ' + request.url, { counter })
+	response.writeHead(200)
+	response.end('Hello World from localhost! ' + counter)
+}
+
+var www = http.createServer(handleRequest)
+www.listen(8080)
